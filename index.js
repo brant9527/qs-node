effa5d78b72fdc7c148e55683583f785f6c76183
brant9527 <398707650@qq.com>
2019-01-14 00:25:10 +0800

    新增 修复页面404问题

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let express = require('express')
 let app = express()
 const path = require('path')
+let history = require('connect-history-api-fallback');
+app.use(history())
+history({
+    index: '/static/index.html'
+});
 app.use(express.static('static'))
 app.listen(8080,'0.0.0.0',function(){
     console.log(1)
